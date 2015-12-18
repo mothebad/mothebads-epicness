567604e3bb419e1cd4a718cbc819ab6bf826e8f4
mothebad <jumbriac@stu.emerson-school.org>
2015-12-18 15:59:17 -0500

    Create play.py

---
+++ play.py
@@ -0,0 +1,28 @@
+#/bin/python3
+#print text:
+#print("")
+#wait for user to hit enter:
+#input()
+#set a variable:
+#variable name = value
+#example:
+#x=10
+#yhd="Hello"
+def clear():
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+def printc(text):
+    clear()
+    print(text)
+printc(":-) !!!!!!! aeofhadigfhjkrbfhbljdbshgr")
+input()
+printc("Please type a number")
+number=int(input())
+printc(number)
+input()
+if number<100 and number>0:
+    printc(":-)")
+elif number<0:
+    printc(":-(")
+else:
+    printc(":-|")
+input()
